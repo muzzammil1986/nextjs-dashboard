a7ac1f7daf472322eb3fee8810346a1b41f13051
muzzammil1986 <muzzammil1986@gmail.com>
2024-06-08 23:32:52 +0400

    google ads update

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,7 @@ export default function RootLayout({
       <head>
         
       <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1599870932013624"
-      crossOrigin="anonymous"
-      strategy="beforeInteractive">
+      crossOrigin="anonymous">
      </Script>
     
       </head>
